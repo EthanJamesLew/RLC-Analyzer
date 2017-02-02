f79945ade30ecd444ac4e5a11ca72fb0c0dfa700
Ethan Lew <ethan.lew@pcc.edu>
2017-02-01 20:12:12 -0800

    Format Correction

--- rlc_analyzer.m
+++ rlc_analyzer.m
@@ -245,7 +245,7 @@ ylim(handles.graph, [ymin-incs ymax+incs]);
 
 for idx = 1:numel(locs_max)
     loc = locs_max(idx);
-    strmax = ['',num2str(round(y(loc), 3))];
+    strmax = ['',num2str(y(loc))];
     text(x(loc),incs/2+y(loc),strmax);
     plot(x(loc), y(loc), 'o', 'LineWidth',1,'MarkerEdgeColor',...
     [ 0    0.4470    0.7410],'MarkerFaceColor',[ 0    0.4470    0.7410],'MarkerSize',6);
